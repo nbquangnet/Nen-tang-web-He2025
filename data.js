0334b1cd2158d74051e4638c1c8e252600b45b58
Nguyen Bao Quang <baoquangnguyen104@gmail.com>
2025-08-03 21:31:13 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+const employees = [
+  {
+    id: 1,
+    name: "Thomas Hardy",
+    email: "thomashardy@mail.com",
+    address: "89 Chiaroscuro Rd, Portland, USA",
+    phone: "0171555222"
+  },
+  {
+    id: 2,
+    name: "Dominique Perrier",
+    email: "dominiqueperrier@mail.com",
+    address: "Obere Str. 57, Berlin, Germany",
+    phone: "0313555735"
+  },
+  {
+    id: 3,
+    name: "Maria Anders",
+    email: "mariaanders@mail.com",
+    address: "Obere Str. 57, Berlin, Germany",
+    phone: "0987654321"
+  },
+  {
+    id: 4,
+    name: "Francisco Chang",
+    email: "franciscochang@mail.com",
+    address: "Mexico City, Mexico",
+    phone: "0912345678"
+  },
+  {
+    id: 5,
+    name: "Nguyen Bao Quang",
+    email: "quangnguyenbao@mail.com",
+    address: "Thanh Xuan, Hano, Vietnam",
+    phone: "0909090909"
+  }
+];
